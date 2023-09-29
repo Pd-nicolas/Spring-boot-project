c74a1f8a0165c8b4da45533d9d6b80dbf876c1d7
Pd-nicolas <Pedronicks25@gmail.com>
2023-09-29 16:43:17 -0300

    PedidoRequestDto adicionado

--- src/main/java/com/example/crud/dto/request/PedidoRequestDto.java
+++ src/main/java/com/example/crud/dto/request/PedidoRequestDto.java
@@ -1,4 +1,19 @@
 package com.example.crud.dto.request;
 
+import com.example.crud.entity.Cliente;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class PedidoRequestDto {
+    @NotNull(message = "É obrigatório o preenchimento do campo data")
+    private Date data;
+
+    @NotNull(message = "É obrigatório o preenchimento do campo total")
+    private BigDecimal total;
+
+    @NotEmpty(message = "É obrigatório o preenchimento do campo cliente")
+    private Cliente cliente;
 }
