a9254950e90e777702e6876e50eab0774359e944
Pd-nicolas <Pedronicks25@gmail.com>
2023-09-29 16:36:52 -0300

    itemPedidoResponseDto adcionado

--- src/main/java/com/example/crud/dto/request/ItemPedidoRequestDto.java
+++ src/main/java/com/example/crud/dto/request/ItemPedidoRequestDto.java
@@ -1,4 +1,17 @@
 package com.example.crud.dto.request;
 
+import com.example.crud.entity.Pedido;
+import com.example.crud.entity.Produto;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class ItemPedidoRequestDto {
+    @NotNull(message = "É obrigatório o preenchimento do campo quantidade")
+    private Integer quantidade;
+
+    @NotEmpty(message = "É obrigatório o preenchimento do campo pedido")
+    private Pedido pedido;
+
+    @NotEmpty(message = "É obrigatório o preenchimento do campo pedido")
+    private Produto produto;
 }
