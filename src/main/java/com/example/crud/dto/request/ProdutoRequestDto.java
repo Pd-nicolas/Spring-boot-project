f0ba098c1b44bf9803bde9a8923e219c7f061565
Pd-nicolas <Pedronicks25@gmail.com>
2023-09-29 16:46:05 -0300

    ProdutoRequestDto adiocionado

--- src/main/java/com/example/crud/dto/request/ProdutoRequestDto.java
+++ src/main/java/com/example/crud/dto/request/ProdutoRequestDto.java
@@ -1,4 +1,14 @@
 package com.example.crud.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
 public class ProdutoRequestDto {
+    @NotEmpty(message = "É necessário preencher o campo descricao")
+    private String descricao;
+
+    @NotNull(message = "É necessário preencher o campo preco")
+    private BigDecimal preco;
 }
